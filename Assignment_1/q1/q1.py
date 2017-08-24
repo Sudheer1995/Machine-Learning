5abab011f92cfe600029577985b09e524e0dab5f
Sudheer1995 <sudheerachary115@gmail.com>
2017-08-25 01:53:45 +0530

    beta version commit

--- Assignment_1/q1/q1.py
+++ Assignment_1/q1/q1.py
@@ -7,90 +7,68 @@ import sys
 import time
 
 def load_data(train_file, test_file):
-
+	"""loads data from csv file"""
 	with open(train_file) as f:
-
 		reader = csv.reader(f, delimiter=' ', quotechar='|')
 		data = list(reader)
 		samples = len(data)
-
 		train_X = numpy.zeros((samples, 784), dtype=numpy.int)
 		train_Y = numpy.zeros((samples, 1), dtype=numpy.int)
-
 		for i in range(samples):
-
 			row = map(int, data[i][0].split(','))
 			train_Y[i, 0] = row[0]
 			train_X[i, :] = numpy.asarray(row[1: ])
-
 		train_X = numpy.append(numpy.ones((train_X.shape[0], 1)), train_X, axis=1)
 
 	with open(test_file) as f:
-
 		reader = csv.reader(f, delimiter=' ', quotechar='|')
 		data = list(reader)
 		samples = len(data)
-
 		test_X = numpy.zeros((samples, 784), dtype=int)
 		test_Y = numpy.zeros((samples, 1), dtype=int)
-
 		for i in range(samples):
-
 			row = map(int, data[i][0].split(','))
 			test_Y[i, 0] = row[0]
 			test_X[i, :] = numpy.asarray(row[1: ])
-
 		test_X = numpy.append(numpy.ones((test_X.shape[0], 1)), test_X, axis=1)
+		
 		return (train_X, train_Y, test_X, test_Y)
 
 def train_weights(data, labels, sm_margin, bm_margin):
-
+	"""trains weights for four perceptrons"""
 	epoch = 0
 	labels = 2*labels - 1	
 	data = numpy.multiply(labels, data)
-	
 	s_weights = numpy.ones((data.shape[1], 1), dtype=numpy.float32)
 	sm_weights = numpy.ones((data.shape[1], 1), dtype=numpy.float32)
 	b_weights = numpy.ones((data.shape[1], 1), dtype=numpy.float32)
 	bm_weights = numpy.ones((data.shape[1], 1), dtype=numpy.float32)
-	
 	while epoch < 100:
-
 		for i in range(data.shape[0]):
-			
 			s_error = numpy.matmul(data[i, :].reshape(1, data.shape[1]), s_weights)
 			sm_error = numpy.matmul(data[i, :].reshape(1, data.shape[1]), sm_weights)
-
 			if s_error[0, 0] < 0:
 				s_weights = numpy.add(s_weights, data[i, :].reshape(s_weights.shape[0], 1))
-
 			if sm_error[0, 0] < sm_margin:
 				sm_weights = numpy.add(sm_weights, data[i, :].reshape(sm_weights.shape[0], 1))
 
-
 		b_errors = numpy.matmul(data, b_weights)
 		bm_errors = numpy.matmul(data, bm_weights)
-
 		bm_wrong_vecs, j = numpy.where(bm_errors < bm_margin)
 		b_wrong_vecs, j = numpy.where(b_errors < 0)
-
 		for k in bm_wrong_vecs:
-			
 			misclassified = data[k, :].reshape(bm_weights.shape[0], 1)
 			bm_weights = numpy.add(bm_weights, misclassified)
-		
+
 		for k in b_wrong_vecs:
-			
 			misclassified = data[k, :].reshape(b_weights.shape[0], 1)
 			b_weights = numpy.add(b_weights, misclassified)
-		
-
 		epoch += 1
 
 	return (s_weights, sm_weights, b_weights, bm_weights)
 
 def test_weights(weights, data, labels, margin):
-		
+	"""returns results on test data"""
 	errors = numpy.matmul(data, weights)
 	labels = 2*labels-1
 	signs = numpy.multiply(errors, labels)
--- Assignment_1/q2/q2.py
+++ Assignment_1/q2/q2.py
@@ -8,18 +8,14 @@ import sys
 import time
 
 def load_data(train_file):
-
+	"""loads data from csv file"""
 	with open(train_file) as f:
-
 		reader = csv.reader(f, delimiter=' ', quotechar='|')
 		data = list(reader)
 		samples = len(data)
-
 		train_X = numpy.zeros((samples, 8), dtype=numpy.float32)
 		train_Y = numpy.zeros((samples, 1), dtype=numpy.float32)
-
 		for i in range(samples):
-
 			row = map(numpy.float32, data[i][0].split(','))
 			train_Y[i, 0] = row[10]
 			train_X[i, :] = numpy.asarray(row[1: 9])
@@ -27,7 +23,6 @@ def load_data(train_file):
 		test_X = train_X[int(3*samples/4)+1: , :]
 		test_X = numpy.append(numpy.ones((test_X.shape[0], 1)), test_X, axis=1)
 		test_Y = train_Y[int(3*samples/4)+1: , :]
-
 		train_X = train_X[:int(3*samples/4), :]
 		train_X = numpy.append(numpy.ones((train_X.shape[0], 1)), train_X, axis=1)
 		train_Y = train_Y[:int(3*samples/4), :]
@@ -35,35 +30,28 @@ def load_data(train_file):
 		return train_X, train_Y, test_X, test_Y
 
 def train_weights(data, labels, margin, ep):
-	
+	"""trains weights for batch perceptron with relaxation & margin"""
 	epoch = 0
 	labels -= 3
 	data = numpy.multiply(labels, data)
 	prev_i = range(data.shape[0])
 	weights = numpy.random.rand(data.shape[1], 1)
 	while epoch <= ep:
-		
 		errors = numpy.matmul(data, weights)
 		i, j = numpy.where(errors < margin)
-		
 		if len(prev_i) > len(i):
 			for k in i:
-				
 				misclassified = data[k, :].reshape(weights.shape[0], 1)
-				error = numpy.matmul(
-								numpy.transpose(weights),
-								misclassified
-								)
+				error = numpy.matmul(numpy.transpose(weights), misclassified)
 				Z = misclassified * (margin - error[0, 0])/(linalg.norm(misclassified)**2)
 				weights = numpy.add(weights, Z)
 			prev_i = i
-
 		epoch += 1
 
 	return weights, (1-len(prev_i)/data.shape[0])*100
 
 def test_weights(weights, data, labels, margin):
-	
+	"""returns results on test data"""
 	labels -= 3
 	errors = numpy.matmul(data, weights)
 	signs = numpy.multiply(errors, labels)
