8ee882d3a5b1267e706ed3328e53aa14a0a2baac
Sudheer1995 <sudheerachary115@gmail.com>
2017-10-01 22:03:03 +0530

    added q3 q4

---
+++ Assignment_2/Q3/q3.py
@@ -0,0 +1,40 @@
+from sklearn.metrics import accuracy_score
+from sklearn import linear_model
+from numpy import genfromtxt
+from random import randint
+import cv2
+import math
+import numpy as np
+
+def showarray(weights):
+	weights = np.asarray(weights).reshape((28, 28))
+	for i in range(weights.shape[0]):
+		for j in range(weights.shape[1]):
+			weights[i, j] = math.fabs(weights[i, j])*10000
+	cv2.imwrite('weights.jpg', weights)
+
+def get_data():
+	X_train = genfromtxt('q3/notMNIST_train_data.csv', delimiter=',')
+	y_train = genfromtxt('q3/notMNIST_train_labels.csv', delimiter=',')
+	X_test = genfromtxt('q3/notMNIST_test_data.csv', delimiter=',')
+	y_test = genfromtxt('q3/notMNIST_test_labels.csv', delimiter=',')
+	return X_train, y_train, X_test, y_test
+
+def train(X, y):
+	model = linear_model.LogisticRegression(penalty='l2', dual=False, tol=0.0001, C=0.0001, 
+		fit_intercept=True, intercept_scaling=1, class_weight=None, random_state=None, 
+		solver='liblinear', max_iter=100, multi_class='ovr', verbose=0, warm_start=False, n_jobs=1)
+	model.fit(X, y, sample_weight=None)
+	return model
+
+def test(model, X, y):
+	y_pred = model.predict(X)
+	return accuracy_score(y, y_pred,  normalize=True, sample_weight=None)
+
+if __name__ == '__main__':
+	n = randint(1, 1000)
+	X_train, y_train, X_test, y_test = get_data()
+	model = train(X_train, y_train)
+	score = test(model, X_test, y_test)
+	showarray(model.coef_)
+	print "--Accuracy--: ", score
---
+++ Assignment_2/Q4/q4_b.py
@@ -0,0 +1,47 @@
+from sklearn import linear_model
+from sklearn.metrics import accuracy_score
+import csv
+import sys
+import numpy as np
+
+def get_data(train_set, test_set):
+	with open(train_set) as train_file:
+		train = []
+		train_data = csv.reader(train_file, delimiter=',', quotechar='|')	
+		for row in train_data:
+			train.append([float(elem) for elem in row])
+		train = np.asarray(train)
+		train_X = train[:, :-1]
+		train_Y = train[:, -1]
+
+	with open(test_set) as test_file:
+		test = []	
+		test_data = csv.reader(test_file, delimiter=',', quotechar='|')
+		for row in test_data:
+			test.append([float(elem) for elem in row])
+		test = np.asarray(test)
+		test_X = test[:, :-1]
+		test_Y = test[:, -1]
+
+	return train_X, train_Y, test_X, test_Y
+
+def train(X, y):
+	# Ridge
+	lin_reg = linear_model.Ridge(alpha=0.00001, fit_intercept=True, normalize=False, 
+		copy_X=True, max_iter=None, tol=0.001, solver='auto', random_state=None)
+	lin_reg.fit(X, y)
+	return lin_reg
+
+def test(model, X, y):
+	y_pred = model.predict(X)
+	y_pred = np.asarray([1 if y_pred[i] > 0.5 else 0 for i in range(y_pred.shape[0])])
+	return accuracy_score(y, y_pred, normalize=True, sample_weight=None)*100
+
+if __name__ == '__main__':
+
+	train_file = sys.argv[1]
+	test_file = sys.argv[2]
+	train_X, train_Y, test_X, test_Y = get_data(train_file, test_file)
+	model = train(train_X, train_Y)
+	accuracy = test(model, test_X, test_Y)
+	print "--Accuracy--: ", accuracy
